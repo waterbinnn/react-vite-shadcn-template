fed38983e3911e4921af06b7e6a6b991d08d6dea
Subin Lee <ewaterbinn@gmail.com>
2025-04-02 01:01:37 +0900

    chore: tsconfig 및 vite.config alias 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
