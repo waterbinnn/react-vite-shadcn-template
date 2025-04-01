a1d2ee42e012d6e820bc09e1933de3ee021802da
Subin Lee <ewaterbinn@gmail.com>
2025-04-02 00:08:36 +0900

    chore: prettier 및 esLint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,17 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import tailwind from 'eslint-plugin-tailwindcss';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tailwind.configs['flat/recommended'],
+    ],
     files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,10 +22,27 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+
+    settings: {
+      tailwindcss: {
+        callees: ['classnames', 'clsx', 'ctl'],
+        config: 'tailwind.config.js',
+        cssFiles: ['**/*.css', '!**/node_modules', '!**/.*', '!**/dist', '!**/build'],
+        cssFilesRefreshRate: 5_000,
+        removeDuplicates: true,
+        skipClassAttribute: false,
+        whitelist: [],
+        tags: [],
+        classRegex: '^class(Name)?$',
+      },
     },
   },
 );
